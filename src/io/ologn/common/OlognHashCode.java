784b355a61d7f59d68f0cd2e857f18f9edadf1ba
lisq199 <Admin@MacBook-Pro.local>
2016-02-24 22:24:43 -0800

    new features

---
+++ src/io/ologn/common/OlognHashCode.java
@@ -0,0 +1,132 @@
+package io.ologn.common;
+
+import java.util.Arrays;
+
+/**
+ * For easily generating hash codes.<br>
+ * Source of algorithm: <a href="http://developer.android.com/reference/java/lang/Object.html">link</a>
+ * <br>
+ * The methods are pretty self-explanatory.<br>
+ * Typical usage: {@code OlognHashCode.init().addInt(a).addArray(b).get()}
+ * @author lisq199
+ */
+public class OlognHashCode {
+	
+	protected int result;
+	protected int step;
+	
+	protected OlognHashCode() {
+		result = 37;
+		step = 31;
+	}
+	
+	protected OlognHashCode(int start, int step) {
+		this.result = start;
+		this.step = step;
+	}
+	
+	/**
+	 * Get the final result
+	 * @return
+	 */
+	public int get() {
+		return result;
+	}
+	
+	public OlognHashCode addBoolean(boolean f) {
+		return addHash(f ? 1 : 0);
+	}
+	
+	public OlognHashCode addByte(byte f) {
+		return addHash(f);
+	}
+	
+	public OlognHashCode addChar(char f) {
+		return addHash(f);
+	}
+	
+	public OlognHashCode addShort(short f) {
+		return addHash(f);
+	}
+	
+	public OlognHashCode addInt(int f) {
+		return addHash(f);
+	}
+	
+	public OlognHashCode addLong(long f) {
+		return addHash((int) (f ^ (f >>> 32)));
+	}
+	
+	public OlognHashCode addFloat(float f) {
+		return addHash(Float.floatToIntBits(f));
+	}
+	
+	public OlognHashCode addDouble(double f) {
+		return addLong(Double.doubleToLongBits(f));
+	}
+	
+	public OlognHashCode addArray(boolean[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(byte[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(char[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(short[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(int[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(long[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(float[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(double[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addArray(Object[] f) {
+		return addHash(Arrays.hashCode(f));
+	}
+	
+	public OlognHashCode addObject(Object f) {
+		return addHash(f == null ? 0 : f.hashCode());
+	}
+
+	public OlognHashCode addHash(int hash) {
+		result = step * result + hash;
+		return this;
+	}
+	
+	public static OlognHashCode init() {
+		return new OlognHashCode();
+	}
+	
+	/**
+	 * Initialize with specified start and step values
+	 * @param start the value the resulting hash code starts with
+	 * @param step the value that is going to be multiplied each time 
+	 * a new field is added.
+	 * @return
+	 */
+	public static OlognHashCode init(int start, int step) {
+		if (start == 0 || step == 0) {
+			throw new IllegalArgumentException(
+					"start and step must be non-zero");
+		}
+		return new OlognHashCode(start, step);
+	}
+
+}
--- src/io/ologn/common/math/OlognMath.java
+++ src/io/ologn/common/math/OlognMath.java
@@ -7,6 +7,29 @@ package io.ologn.common.math;
 public class OlognMath {
 	
 	/**
+	 * Check if an int is a prime number.<br>
+	 * Source of algorithm: <a href="http://www.mkyong.com/java/how-to-determine-a-prime-number-in-java/">link</a>
+	 * @param n
+	 * @return
+	 */
+	public static boolean isPrime(int n) {
+		if (n == 2) {
+			return true;
+		}
+		//check if n is a multiple of 2
+		if (n < 2 || n % 2 == 0) {
+			return false;
+		}
+		//if not, then just check the odds
+		for (int i = 3; i * i <= n; i += 2) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
 	 * Check if a is between b and c (exclusive).
 	 * @param a
 	 * @param b
@@ -38,6 +61,10 @@ public class OlognMath {
 		} else {
 			return c > b ? a > b && a < c : a > c && a < b;
 		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isPrime(2));
 	}
 	
 }
